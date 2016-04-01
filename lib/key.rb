b386a0e9021f69c2be180ca6b5afc496e8b69df5
Andrew Fink <andrewfink8@gmail.com>
2016-04-01 08:47:39 -0700

    finish key tests

--- lib/key.rb
+++ lib/key.rb
@@ -1,2 +1,25 @@
+require "byebug"
 class Key
+
+  attr_reader :key
+
+  def initialize(key = "00000")
+    @key = key.to_s.rjust(5,"0")
+  end
+
+  def rotations
+    rotations_output = []
+    @key.chars.each_cons(2) {|x| rotations_output << x}
+    result = rotations_output.map {|x| x.join("").to_i}
+    result
+  end
+
+  def self.generate
+    new_key = []
+    5.times do
+      new_key << rand(9)
+    end
+  new_key.join("")
+  end
+
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -5,9 +5,90 @@ require './lib/key'
 
 class KeyTest < Minitest::Test
 
-  def test_it_can_hold_a_key
-    result = Key.new
-    assert_equal "#<Key", result.itself.to_s.chars.first(5).join("")
-  end
+    def test_it_can_hold_a_key
+      result = Key.new
+      assert_equal "#<Key", result.itself.to_s.chars.first(5).join("")
+    end
 
+    def test_it_has_a_method_to_calculate_rotations
+      result = Key.new
+      assert result.respond_to?(:rotations)
+    end
+
+    def test_it_has_a_method_to_generate_a_key
+      assert Key.respond_to?(:generate)
+    end
+
+    def test_it_has_a_default_key_value_if_none_given
+      result = Key.new
+      assert_equal "00000", result.key
+    end
+
+    def test_it_correctly_formats_a_key_less_than_five_digits
+      result = Key.new("5")
+      assert_equal "00005", result.key
+    end
+
+    def test_it_correctly_formats_a_key_given_as_integer
+      result = Key.new(12345)
+      assert_equal "12345", result.key
+    end
+
+    def test_it_can_accept_a_known_key
+      result = Key.new("12345")
+      assert_equal "12345", result.key
+    end
+
+    def test_it_can_generate_a_five_digit_key
+      assert_equal 5, Key.generate.length
+    end
+
+    def test_it_can_generate_a_random_five_digit_key
+      keys = []
+      1000.times do
+        keys << Key.generate
+      end
+      keys.group_by{|key| key}
+      assert_equal 1000, keys.count
+    end
+
+    def test_it_returns_rotation_values_in_an_array
+      result = Key.new("12345")
+      assert result.rotations.is_a?Array
+    end
+
+    def test_it_can_give_the_rotation_for_the_first_number
+
+      result = Key.new("12345")
+      rotation = result.rotations
+      assert_equal 12, rotation[0]
+    end
+
+    def test_it_can_give_the_rotation_for_the_second_number
+
+      result = Key.new("12345")
+      rotation = result.rotations
+      assert_equal 23, rotation[1]
+    end
+
+    def test_it_can_give_the_rotation_for_the_third_number
+
+      result = Key.new("12345")
+      rotation = result.rotations
+      assert_equal 34, rotation[2]
+    end
+
+    def test_it_can_give_the_rotation_for_the_fourth_number
+
+      result = Key.new("12345")
+      rotation = result.rotations
+      assert_equal 45, rotation[3]
+    end
+
+    def test_it_can_give_all_four_rotation_values
+
+      result = Key.new("41521")
+      rotation = result.rotations
+      assert_equal [41, 15, 52, 21], rotation
+    end
 end
