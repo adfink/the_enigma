4523b18a4efd7a5be6d823522732bebbecefdfb5
Andrew Fink <andrewfink8@gmail.com>
2016-04-01 09:28:58 -0700

    rotations test pass

---
+++ lib/total_rotations.rb
@@ -0,0 +1,36 @@
+require './lib/key'
+require './lib/offset'
+
+class TotalRotations
+
+  attr_reader :key, :date
+  def initialize(key = Key.new, date = Offset.new)
+    @key = Key.new(key)
+    @date = Offset.new(date)
+  end
+
+  def sum
+    date.offsets.zip(key.rotations).map {|combined|combined.reduce(:+)}
+  end
+
+
+
+end
+
+
+# require './lib/key'
+# require './lib/offset'
+#
+# # Sums the key and offset array values.
+# class TotalRotations
+#   attr_reader :key, :date
+#
+#   def initialize(key = Key.new, date = Offset.new)
+#     @key = Key.new(key)
+#     @date = Offset.new(date)
+#   end
+#
+#   def sum
+#     key.rotations.zip(date.offsets).map {|pair| pair.reduce(:+)}
+#   end
+# end
